2536cbc60877e2ec4f83d98a02934dcb06d44b05
EdwinEinsen <edwin.vasquez@pos.red>
2021-09-10 03:39:14 -0600

    Template, directivas, data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'apm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
